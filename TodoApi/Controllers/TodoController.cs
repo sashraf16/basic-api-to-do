3f11daa5da58e55c8e192206cb17b2cc07b646ed
sashraf16 <deadlyrang@gmail.com>
2018-02-22 20:38:21 -0500

    implement crud

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -8,17 +8,40 @@ namespace TodoApi.Controllers
     [Route("api/[controller]")]
     public class TodoController : Controller
     {
-        private readonly TodoContext _context;
+        private readonly TodoContext _context;  
 
-        public TodoController(TodoContext context)
+        public TodoController (TodoContext context)
         {
             _context = context;
 
             if (_context.TodoItems.Count() == 0)
             {
-                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item1"});
                 _context.SaveChanges();
             }
-        }       
+        } 
+
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault( t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+
+
     }
 }
